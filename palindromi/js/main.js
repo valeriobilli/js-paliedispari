b86108f1ae4b73eb82d2898acae21ad6469b665f
Valerio Billi <valerio.billi18@gmail.com>
2023-04-26 15:18:59 +0200

    first commit

---
+++ palindromi/js/main.js
@@ -0,0 +1,23 @@
+let parola = prompt("Inserisci una parola");
+console.log("Hai scelto:", parola);
+
+let parolaReverse = invertiParola(parola);
+console.log("Parola invertita: ", parolaReverse);
+
+if(parola.toLowerCase() == parolaReverse.toLowerCase()) {
+    alert("La parola è palindroma! 👍🏻")
+} else {
+    alert("La parola non è palindroma... 👎🏻")
+}
+
+function invertiParola(parolaDaInvertire) {
+    let parolaInvertita = "";
+
+    for (let i = parolaDaInvertire.length - 1; i >= 0; i--) {
+        let carattere = parolaDaInvertire[i];
+        parolaInvertita += carattere;
+        
+    }
+
+    return parolaInvertita;
+}
---
+++ pari-dispari/js/main.js
@@ -0,0 +1,59 @@
+let inputUtenteOk = false;
+let numeroUtente;
+
+while( inputUtenteOk==false ) {
+
+    numeroUtente = parseInt(prompt("Scegli un numero tra 1 e 5"));
+    let numeroNonENan = false;
+    let numeroENelRange = false;
+
+    if (isNaN(numeroUtente) == false) {
+        numeroNonENan = true;
+    }
+
+    if( numeroUtente>=1 && numeroUtente<=5 ) {
+        numeroENelRange = true;
+    }
+
+    if (numeroNonENan && numeroENelRange) {
+        inputUtenteOk = true; 
+    }
+}
+
+let numeroComputer = getRandomNumber(1, 5);
+
+function getRandomNumber(min, max) {
+    return Math.floor( Math.random() * (max - min +1) + min );
+}
+
+console.log(`Numero utente: ${numeroUtente} - Numero computer: ${numeroComputer}`)
+
+
+let previsioneUtente = prompt("Pari o dispari?");
+previsioneUtente = previsioneUtente.toLocaleLowerCase();
+
+console.log(`Hai scelto: ${previsioneUtente}`);
+
+
+let somma = numeroUtente + numeroComputer;
+
+let esito = getPariOrDispari(somma);
+
+function getPariOrDispari(numero) {
+   if (numero % 2 == 0)
+   {
+    return "pari";
+   }
+
+   return "dispari";
+}
+
+console.log(`Il numero ${somma} è ${esito}`);
+
+if(esito.toLocaleLowerCase() == previsioneUtente) {
+    alert("Hai vinto! 😎");
+    console.log("Hai vinto!")
+} else {
+    alert("Hai perso... 😭");
+    console.log("Hai perso...")
+}
